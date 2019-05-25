395aa060deb8a9d478c8b586024c3550fece4759
Ben Thomas <ben@nzgeek.co.nz>
2019-05-25 12:46:42 +1200

    Fixing parameter issues

--- Powershell/Scripts/CAU_S2D_PreNodePatching.ps1
+++ Powershell/Scripts/CAU_S2D_PreNodePatching.ps1
@@ -37,13 +37,13 @@ if ( $ClusterPhysicalDisks.where(
 # Enter Storage Maintenance Mode
 try {
     # Suspend cluster node first, otherwise CAU will fail to pause it after this script finishes
-    Suspend-ClusterNode -Name $ComputerName -Drain -RetryDrainOnFailure -Wait -Confirm:$false -ErrorAction Stop
+    Suspend-ClusterNode -Name $ComputerName -Drain:$true -RetryDrainOnFailure:$true -Wait:$true -Confirm:$false -ErrorAction Stop
     # Actually enable maintenance mode
     $ScaleUnit | Enable-StorageMaintenanceMode -ErrorAction Stop
 }
 catch {
     # Make sure node is brought back out of maintenance
     $ScaleUnit | Disable-StorageMaintenanceMode 
-    Resume-ClusterNode -Name $ComputerName -Failback -Confirm:$false
+    Resume-ClusterNode -Name $ComputerName -Failback:$true -Confirm:$false
     throw "Failed to enter storage maintenance mode, try again"
 }
