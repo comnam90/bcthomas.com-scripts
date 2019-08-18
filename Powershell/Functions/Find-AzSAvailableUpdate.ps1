c27dcf7a7d4c13964ed97fb0269c3335b92e2589
Ben Thomas <ben@nzgeek.co.nz>
2019-08-18 18:13:15 +1200

    Update Find-AzSAvailableUpdate.ps1
     - Added begin{}, process{}, end{}
     - Changed from String to Object default output
     - Added 'SimpleOutput' switch to return original output

--- Powershell/Functions/Find-AzSAvailableUpdate.ps1
+++ Powershell/Functions/Find-AzSAvailableUpdate.ps1
@@ -18,11 +18,15 @@ function Find-AzSAvailableUpdates {
         This will return only the latest available update version.
         .Notes
         ----------------------------------------------------------
-        Version: 1.0.0
+        Version: 1.1.0
         Maintained By: Ben Thomas (@NZ_BenThomas)
-        Last Updated: 2019-08-16
+        Last Updated: 2019-08-18
         ----------------------------------------------------------
         CHANGELOG:
+            1.1.0
+             - Introduced Begin,Process,End
+             - Changed to returning an object by default
+             - Added switch for Simple output
             1.0.0
              - Initial Version
     #>
@@ -32,67 +36,119 @@ function Find-AzSAvailableUpdates {
         [ValidatePattern("^\d\.\d{4}\.\d+\.\d+$")]
         [string]$Version,
         [parameter(ParameterSetName = "LatestOnly")]
-        [Switch]$LatestOnly
+        [Switch]$LatestOnly,
+        [Switch]$SimpleOutput
     )
 
-    # Establishing Variables
-    $publishedUpdates = @{ }
-    $AvailableUpdates = @()
-    $URI = 'https://aka.ms/azurestackautomaticupdate'
+    begin {
+        # Establishing Variables
+        $publishedUpdates = @{ }
+        $AvailableUpdates = @()
+        $URI = 'https://aka.ms/azurestackautomaticupdate'
+        $IsLatest = $false
 
-    try {
-        Write-Verbose "Attempting to get AzureStack Update list from $URI"
-        $Content = Invoke-RestMethod -Method Get -UseBasicParsing -Uri $URI -ErrorAction Stop
-    } catch {
-        Write-Warning "Failed to get update list from $URI"
-        Throw "$($PSItem.ToString())"
+        try {
+            Write-Verbose "Attempting to get AzureStack Update list from $URI"
+            $Content = Invoke-RestMethod -Method Get -UseBasicParsing -Uri $URI -ErrorAction Stop
+        } catch {
+            Write-Warning "Failed to get update list from $URI"
+            Throw "$($PSItem.ToString())"
+        }
     }
 
-    $Object = [xml]$Content.Substring(3)
-    $UpdateCount = $Object.AzureStackUpdates.CurrentVersion.Count
-    Write-Verbose "Found $($UpdateCount) updates"
+    process {
+        $Object = [xml]$Content.Substring(3)
+        $UpdateCount = $Object.AzureStackUpdates.CurrentVersion.Count
+        Write-Verbose "Found $($UpdateCount) updates"
 
-    $latestUpdate = (
-        $Object.AzureStackUpdates.CurrentVersion | 
-        Sort-Object -Property Version | 
-        Select-Object -Last 1
-    ).ApplicableUpdate.Version
+        $latestUpdate = (
+            $Object.AzureStackUpdates.CurrentVersion | 
+            Sort-Object -Property Version | 
+            Select-Object -Last 1
+        ).ApplicableUpdate.Version
 
-    if ($PSCmdlet.ParameterSetName -eq "AllUpdates") {
-        Write-Verbose "Creating hashtable with the updates"
-        $Object.AzureStackUpdates.CurrentVersion.Foreach{
-            $publishedUpdates[$PSItem.Version] = $PSItem.ApplicableUpdate
-        }
-        Write-Verbose "Checking if $Version has any updates"
+        if ($PSCmdlet.ParameterSetName -eq "AllUpdates") {
+            Write-Verbose "Creating hashtable with the updates"
+            $Object.AzureStackUpdates.CurrentVersion.Foreach{
+                $publishedUpdates[$PSItem.Version] = $PSItem.ApplicableUpdate
+            }
+            Write-Verbose "Checking if $Version has any updates"
         
-        if (!$publishedUpdates[$Version]) {
-            Write-Verbose "No updates available`nChecking if it's already on the latest version"
-            if ($Version -ne $latestUpdate) {
-                Write-Error "$Version is not a valid AzureStack revision"
+            if (!$publishedUpdates[$Version]) {
+                Write-Verbose "No updates available, checking if it's already on the latest version"
+                if ($Version -ne $latestUpdate) {
+                    Write-Error "$Version is not a valid AzureStack revision"
+                } else {
+                    Write-Verbose "$Version is the newest version available"
+                    $IsLatest = $true
+                }
             } else {
-                Write-Verbose "$Version is the newest version available"
-                Write-Output "No updates available"
+                Write-Verbose "$Version needs to be updated"
+                $UpdatesAvailable = $true
+                $Update = $publishedUpdates[$Version]
+                Write-Verbose "  Found $($Update.Version), checking for more"
+                $UpdateDetails = Invoke-WebRequest -Uri $Update.MetadataFile.Uri -UseBasicParsing | 
+                Select-Object -ExpandProperty Content | ForEach-Object {
+                    ([xml]$_).UpdatePackageManifest.UpdateInfo
+                }
+                $AvailableUpdates += $UpdateDetails
+                do {
+                    $Update = $publishedUpdates[$Update.Version]
+                    if (!$Update) {
+                        $UpdatesAvailable = $false
+                    } else {
+                        Write-Verbose "  Found $($Update.Version), checking for more"
+                        $UpdateDetails = Invoke-WebRequest -Uri $Update.MetadataFile.Uri -UseBasicParsing | 
+                        Select-Object -ExpandProperty Content | ForEach-Object {
+                            ([xml]$_).UpdatePackageManifest.UpdateInfo
+                        }
+                        $AvailableUpdates += $UpdateDetails
+                    }
+                }until(
+                    $UpdatesAvailable -eq $false
+                )
             }
         } else {
-            Write-Verbose "$Version needs to be updated"
-            $UpdatesAvailable = $true
-            $Update = $publishedUpdates[$Version].Version
-            Write-Verbose "  Found $($Update), checking for more"
-            $AvailableUpdates += $Update
-            do {
-                $Update = $publishedUpdates[$Update].Version
-                if (!$Update) {
-                    $UpdatesAvailable = $false
-                } else {
-                    Write-Verbose "  Found $($Update), checking for more"
-                    $AvailableUpdates += $Update
-                }
-            }until(
-                $UpdatesAvailable -eq $false
-            )
-            Write-Output "The following updates are available for AzureStack $($Version):`n$($AvailableUpdates -join ', ')"
+            Write-Verbose "$latestUpdate is the latest AzureStack Update"
+            $IsLatest = $true
+            $DetailsURI = (
+                $Object.AzureStackUpdates.CurrentVersion | 
+                Sort-Object -Property Version | 
+                Select-Object -Last 1
+            ).ApplicableUpdate.MetadataFile.Uri
+            $AvailableUpdates += Invoke-WebRequest -Uri $DetailsURI -UseBasicParsing | 
+            Select-Object -ExpandProperty Content | ForEach-Object {
+                ([xml]$_).UpdatePackageManifest.UpdateInfo
+            }
         }
-    } else {
-        Write-Output "$latestUpdate is the latest AzureStack Update"
+    }
+
+    end {
+        if ($AvailableUpdates.Count -ge 1) {
+            if (!$SimpleOutput) {
+                $AvailableUpdates.ForEach{
+                    $Type = Switch -Wildcard ($PSItem.UpdateName) {
+                        "*Update*" { "Update" }
+                        "*Hotfix*" { "Hotfix" }
+                        default { "Unknown" }
+                    }
+                    [pscustomobject][ordered]@{
+                        Type          = [string]$Type
+                        Name          = [string]$PSItem.UpdateName
+                        Description   = [string]$PSItem.Description
+                        SizeMB        = [uint64]$PSItem.PackageSizeInMB
+                        Version       = [version]$PSitem.Version
+                        PreReqVersion = [version]$PSItem.MinVersionRequired
+                        KBLink        = [uri]$PSItem.KBLink
+                    }
+                }
+            } elseif ($PSCmdlet.ParameterSetName -eq "LatestOnly") {
+                Write-Output "The latest update available: $($AvailableUpdates.Version)"
+            } else {
+                Write-Output "The following updates are available for $($Version):`n$($AvailableUpdates.Version -join ', ')"
+            }
+        } elseif ($IsLatest -and $SimpleOutput) {
+            Write-Output "$Version is up to date"
+        }
     }
 }
